3a5f876e5a465c7ef6e7c292438ef7fea32cf091
stefanpilz <stefan.pilz@apro.at>
2019-10-22 15:27:31 +0200

    Update index.ts
    added keyboard to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { NgVirtualKeyboardDirective } from './virtual-keyboard.directive';
 import { NgVirtualKeyboardModule } from './virtual-keyboard.module';
+import { VirtualKeyboardComponent } from './virtual-keyboard.component';
 
 export {
   NgVirtualKeyboardDirective,
-  NgVirtualKeyboardModule
+  NgVirtualKeyboardModule,
+  VirtualKeyboardComponent
 }
