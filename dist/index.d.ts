f22c14d124e7fc27348e681d6e5cdeeea9e9bf5d
stefanpilz <stefan.pilz@apro.at>
2020-04-07 14:33:23 +0200

    temp

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { NgVirtualKeyboardDirective } from './virtual-keyboard.directive';
-import { NgVirtualOnScreenKeyboardDirective } from './virtual-onscreen-keyboard.directive';
-import { NgVirtualKeyboardModule } from './virtual-keyboard.module';
-import { VirtualKeyboardComponent } from './virtual-keyboard.component';
-export { NgVirtualKeyboardDirective, NgVirtualKeyboardModule, VirtualKeyboardComponent, NgVirtualOnScreenKeyboardDirective, };
